ece662967b09994af4fcc159b31d05557513496b
Faradey27 <andrii.tiertyshnyi@gmail.com>
2017-09-23 17:16:14 +0300

    Added image to card header

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,17 +5,29 @@ const Card = ({ cardData }) => (
     className="card"
     data-testid="card"
   >
+    <div className="card-header">
+      <img
+        height="100%"
+        src={cardData.mainPhoto}
+        width="100%"
+      />
+    </div>
     {cardData.id}
     <style jsx>
       {`
         @import 'theme.css';
         .card {
           display: flex;
+          flex-wrap: wrap;
           height: 535px;
           max-width: 420px;
           width: 100%;
-          box-shadow: var(--boxShadow);
-          background: blue;
+          box-shadow: 0 2px 6px -2px rgba(22,22,22,0.45);
+        }
+        .card-header {
+          width: 100%;
+          max-height: 250px;
+          cursor: zoom-in;
         }
       `}
     </style>
