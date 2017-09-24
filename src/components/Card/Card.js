268e044085ccfa048392358baec34e7bafe69586
Faradey27 <andrii.tiertyshnyi@gmail.com>
2017-09-24 15:14:48 +0300

    Updated card styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,7 +12,38 @@ const Card = ({ cardData }) => (
         width="100%"
       />
     </div>
-    {cardData.id}
+    <div className="card-body">
+      <span className="card-body-title">{'Classical Apartment on Royal Mile'}</span>
+      <p className="description">{`
+        Cair Paravel Farmstead transports you into another realm. Come experience
+        living in a round structure filled with amenities-
+        fully equipped kitchen, deep tub, heating and AC, shared hot tub and seasonal salt water pool.
+        fully equipped kitchen, deep tub, heating and AC, shared hot tub and seasonal salt water pool.
+      `}</p>
+    </div>
+    <div className="card-tags-body">
+      <div className="card-tags-title">{'Tags: '}</div>
+      <div className="card-tags">
+        <div className="card-tag">{'Center'}</div>
+        <div className="card-tag">{'flat'}</div>
+        <div className="card-tag">{'owner'}</div>
+        <div className="card-tag">{'owner'}</div>
+      </div>
+    </div>
+    <div className="card-footer">
+      <div className="card-footer-body">
+        <div className="author">
+          <img
+            className="avatar"
+            src="https://cdn-images-1.medium.com/fit/c/80/80/1*c9lYbCK1CYenT88M-2WicA.png"
+          />
+          <div className="author-group">
+            <span className="author-name">{'The Economist'}</span>
+            <span className="date">{'20 minutes ago'}</span>
+          </div>
+        </div>
+      </div>
+    </div>
     <style jsx>
       {`
         @import 'theme.css';
@@ -26,8 +57,87 @@ const Card = ({ cardData }) => (
         }
         .card-header {
           width: 100%;
-          max-height: 250px;
+          height: 45%;
           cursor: zoom-in;
+        }
+        .card-body {
+          width: 100%;
+          padding: 16px 30px 0 30px;
+        }
+        .card-footer {
+          width: 100%;
+          padding: 8px 30px 0 30px;
+        }
+        .card-footer-body {
+          padding-top: 10px;
+          display: flex;
+          align-items: center;
+          width: 100%;
+        }
+        .card-tags-body {
+          display: flex;
+          align-items: center;
+          padding: 0 30px;
+          max-height: 40px;
+        }
+        .card-tags-title {
+          margin-right: 10px;
+          margin-top: 8px;
+        }
+        .avatar {
+          width: 40px;
+          height: 40px;
+          border-radius: 20px;
+          margin-right: 10px;
+        }
+        .author {
+          display: flex;
+          align-items: center;
+          flex-wrap: nowrap;
+          width: 100%;
+        }
+        .author-name {
+          width: 100%;
+        }
+        .author-group {
+          display: flex;
+          align-items: center;
+          flex-wrap: wrap;
+        }
+        .date {
+          font-size: 12px;
+          color: rgba(0,0,0,.44);
+        }
+        .card-tags {
+          display: flex;
+          width: 100%;
+          flex-wrap: wrap;
+        }
+        .card-tag {
+          margin-top: 8px;
+          display: inline-block;
+          border: 1px solid rgba(0, 0, 0, .44);
+          border-radius: 4px;
+          color: rgba(0, 0, 0, .44);
+          padding: 5px;
+          margin-right: 8px;
+          cursor: pointer;
+          background-color: #f3f3f3;
+        }
+        .card-body-title {
+          font-size: 20px;
+          font-weight: 700;
+        }
+        .description {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 3;
+          line-height: 1.5rem;
+          max-height: 6rem;
+          font-size: 16px;
+          color: rgba(0,0,0,.5);
         }
       `}
     </style>
